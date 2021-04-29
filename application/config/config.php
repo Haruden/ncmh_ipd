33c29acd51a49f9a1fd6070a883c819f7ed4764a
ruahden95 <ruahden95@gmail.com>
2021-04-29 09:18:23 +0800

    config file update

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://localhost/ncmh_ipd';
-$config['base_url'] = 'http://ncmh-is.com/ncmh_ipd';
+$config['base_url'] = 'http://localhost/ncmh_ipd';
+//$config['base_url'] = 'http://ncmh-is.com/ncmh_ipd';
 
 /*
 |--------------------------------------------------------------------------
